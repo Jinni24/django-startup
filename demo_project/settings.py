c14cffac831df19b73fd60b81abd57c44d4b1fba
Jinni24 <71126385+Jinni24@users.noreply.github.com>
2022-04-27 14:42:41 +0900

    Update settings.py

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f9xt48j$ih!$rjb+vwsy%&^t_^g3nzrcvfdh+h-90fcxg5g7_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.180.124.36"]
 
 
 # Application definition
